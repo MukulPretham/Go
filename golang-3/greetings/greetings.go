4f6c97d87881586d51bddfebcad8fd662840f0dd
Mukul Pretham <mukulpretham@gmail.com>
2025-08-09 21:46:12 +0530

    for loop and map ds

--- golang-3/greetings/greetings.go
+++ golang-3/greetings/greetings.go
@@ -1,12 +1,36 @@
 package greetings
 
-import ("fmt"
- "errors")
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
 func Hello(name string) (string,error){
 	if name==""{
 		return "",errors.New("empty string")
 	}
-	message := fmt.Sprintf("Hi %s",name) 
+	message := fmt.Sprintf(randomGreeting(),name) 
 	return message,nil
+}
+
+func randomGreeting()string{
+	greetings:=[]string{
+		"Hi %v",
+		"Hello %v",
+		"welcome %v",
+	}
+	return greetings[rand.Intn(len(greetings))]
+}
+
+func Hellos(names[]string) (map[string]string,error){
+	messages := map[string]string{}
+	for _,name := range names{
+		currMsg,err := Hello(name)
+		if err != nil {
+            return nil, err
+        }
+		messages[name] = currMsg
+	} 
+	return messages,nil
 }
