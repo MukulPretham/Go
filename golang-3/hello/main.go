f85b7c995fc707a4dc893a8354698ca42b058548
Mukul Pretham <mukulpretham@gmail.com>
2025-08-09 21:46:00 +0530

    for loop and map ds

--- golang-3/hello/main.go
+++ golang-3/hello/main.go
@@ -7,9 +7,17 @@ import (
 )
 
 func main(){
-	message,err := greetings.Hello("")
+	names := []string{
+		"Mukul",
+		"Pretham",
+		"Munna",
+	}
+	message,err := greetings.Hellos(names)
 	if(err != nil){
 		log.Fatal(err)
 	}
 	fmt.Print(message)
+	for key, value := range message{
+		fmt.Printf("%s ==> %s \n",key,value)
+	}
 }
